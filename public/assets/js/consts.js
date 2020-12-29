de61e30bef3831b065caf45be9751779c3725786
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-12-29 23:28:58 +0530

    js consts

---
+++ public/assets/js/consts.js
@@ -0,0 +1,21 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Chat consts
+
+const loginBox = document.querySelector('#loginBox');
+const chatBox = document.querySelector('#chatBox');
+const loginBtn = document.querySelector('#loginBtn');
+const loginInput = document.querySelector('#loginInput');
+const loginPass = document.querySelector('#loginPass');
+const displayUser = document.querySelector('#displayUser');
+const sendMsgBtn = document.querySelector('#sendMsgBtn');
+const msgBox = document.querySelector('#msgBox');
+const toUser = document.querySelector('#toUser');
+const ulList = document.querySelector('#ulList');
+
